56bafbe0afd193e5bdb5f2534f47024d909a864a
Aurélien <aurelien.aoustin@epitech.eu>
2020-07-20 17:02:42 +0200

    [MODIF] templates and add them in view.py

--- app/views.py
+++ app/views.py
@@ -8,6 +8,14 @@ app = Flask(__name__)
 def homepage():
     return render_template('homepage.html')
 
+@app.route('/login')
+def login():
+    return 'login page'
+
+@app.route('/register')
+def register():
+    return render_template("register.html")
+
 @app.route('/about')
 def about():
     return 'This is the about page'
